f12be4ba2b7a8f5525f2c021177387cf8902367d
Hirotoshin <hirotoshin0211@gmail.com>
2018-12-02 01:53:54 +0900

    create class Component & func Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 class App extends React.Component {
   public render() {
     return (
-      <div>
-        <h1>hogehoge</h1>
-      </div>
+      <React.Fragment>
+        hogehoge
+      </React.Fragment>
     );
   }
 }
