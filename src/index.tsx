9efe1194e4d3acb56fdd10df1fccbf8e5a5e9e09
Hirotoshin <hirotoshin0211@gmail.com>
2018-12-02 21:04:29 +0900

    create store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { logger } from "redux-logger";
+import App from "./App";
+import "./index.css";
+import rootReducer from "./reducer/reducer";
+import registerServiceWorker from "./registerServiceWorker";
 
+const store = createStore(rootReducer, applyMiddleware(logger));
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
---
+++ src/reducer/index.tsx
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+import count from "./reducer";
+
+export default combineReducers({ count });
